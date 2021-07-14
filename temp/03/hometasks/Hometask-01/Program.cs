b62792c3024fccafea343a4412f667b69cd0fbc9
sdvbjdkjv <riuihuirgfsdeuf.com>
2021-07-14 19:39:20 +0300

    123

--- temp/03/hometasks/Hometask-01/Program.cs
+++ temp/03/hometasks/Hometask-01/Program.cs
@@ -12,13 +12,13 @@ namespace PearlPoet173377.Hometask_01
         static void Main(string[] args)
         {
             char key;
-            int protT = 0;
+            int a = 0;
+            int sizeT = 0;
 
-            int sizeTeam = 0;
             List<Worker> workers = new List<Worker>();
             TeamLeader Leader = null;
-            Team team = new Team(workers, Leader);
             House house = new House();
+            Team team = new Team(workers, Leader);
 
             do
             {
@@ -27,40 +27,53 @@ namespace PearlPoet173377.Hometask_01
                 Console.WriteLine("2. Информация о бригаде.");
                 Console.WriteLine("3. Построить дом");
                 Console.WriteLine("4. Вывести отчёт");
-                Console.Write("Выберете действие: "); key = Console.ReadKey().KeyChar; Console.Clear();
+                Console.Write("Выберете действие: ");
+                key = Console.ReadKey().KeyChar;
+                Console.Clear();
+
                 switch (key)
                 {
                     case '1':
                         {
-                            if (protT == 0)
+                            if (a == 0)
                             {
-                                Console.Write("Введите размер бригады: "); sizeTeam = int.Parse(Console.ReadLine());
-                                if (sizeTeam > 2)
+                                Console.Write("Введите размер бригады: ");
+                                sizeT = int.Parse(Console.ReadLine());
+                                if (sizeT > 2)
                                 {
-                                    Console.Write("Введите имя бригадира: "); Leader = new TeamLeader(Console.ReadLine());
+                                    Console.Write("Введите имя бригадира: ");
+                                    Leader = new TeamLeader(Console.ReadLine());
                                 }
                                 else
                                 {
                                     Leader = null;
-                                    sizeTeam++;
+                                    sizeT++;
                                 }
-                                for (int i = 0; i < sizeTeam - 1; i++)
+                                for (int i = 0; i < sizeT - 1; i++)
                                 {
-                                    Console.Write($"Введите имя рабочего №{ i + 1 }: "); workers.Add(new Worker(Console.ReadLine()));
+                                    Console.Write($"Введите имя рабочего №{ i + 1 }: ");
+                                    workers.Add(new Worker(Console.ReadLine()));
                                 }
                                 team = new Team(workers, Leader);
-                                Console.WriteLine("Бригада создана! Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
-                                protT++;
+                                Console.WriteLine("Бригада создана! Для выхода нажмите любую клавишу...");
+                                Console.ReadKey();
+                                Console.Clear();
+                                a++;
                             }
+
                             else
                             {
-                                Console.WriteLine("Вторую бригаду нельзя добавить!\nДля выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                                Console.WriteLine("Вторую бригаду нельзя добавить!\nДля выхода нажмите любую клавишу...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
                         }
+
                         break;
+
                     case '2':
                         {
-                            if (sizeTeam != 0)
+                            if (sizeT != 0)
                             {
                                 if (TeamLeader.Name != null)
                                 {
@@ -72,60 +85,73 @@ namespace PearlPoet173377.Hometask_01
                                 }
                             }
                             else Console.WriteLine("Бригада не создана!");
-                            Console.WriteLine("Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                            Console.WriteLine("Для выхода нажмите любую клавишу...");
+                            Console.ReadKey();
+                            Console.Clear();
                         }
                         break;
+
                     case '3':
                         {
-                            if (team.report.Count != 11 && sizeTeam != 0)
+                            if (team.report.Count != 11 && sizeT != 0)
                             {
-                                char key2;
+                                char ab;
                                 Console.WriteLine("Нажмите 1 чтобы строить дом. Чтобы выйти нажмите 0");
                                 do
                                 {
-                                    key2 = Console.ReadKey().KeyChar;
-                                    if (key2 == '1')
+                                    ab = Console.ReadKey().KeyChar;
+                                    if (ab == '1')
                                     {
                                         house.Build(house, team);
                                         Console.WriteLine(team.report.Count);
                                     }
-                                    else if (key2 == '0') break;
+                                    else if (ab == '0') break;
                                     if (team.report.Count == 11)
                                     {
                                         Console.Clear();
                                         Console.WriteLine("Ваш дом построен!");
-                                        house.Paint();
                                         break;
                                     }
-                                } while (key2 != '0');
-                                Console.WriteLine("Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                                } while (ab != '0');
+                                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
                             else if (team.report.Count == 11)
                             {
-                                Console.WriteLine("Дом уже построен!"); house.Paint();
-                                Console.WriteLine("Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                                Console.WriteLine("Дом уже построен!");
+                                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
                             else
                             {
                                 Console.WriteLine("Бригада не создана!");
-                                Console.WriteLine("Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
 
                         }
 
                         break;
+
                     case '4':
                         {
-                            if (sizeTeam != 0 && team.report.Count > 0)
+                            if (sizeT != 0 && team.report.Count > 0)
                             {
                                 foreach (var i in team.report)
                                 {
                                     Console.WriteLine(i);
                                 }
                             }
+
                             else Console.WriteLine("Дом еще не строился!");
-                            Console.WriteLine("Для выхода нажмите любую клавишу..."); Console.ReadKey(); Console.Clear();
+                            Console.WriteLine("Для выхода нажмите любую клавишу...");
+                            Console.ReadKey();
+                            Console.Clear();
                         }
+
                         break;
                 }
             } while (key != '0');
